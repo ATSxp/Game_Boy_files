c8ac5bc33986f8572021c64881da6aa8b7de592f
ATS <92697714+ATSxp@users.noreply.github.com>
2022-10-06 23:24:06 -0300

    Add files via upload

---
+++ GBA/template_libtonc/source/main.cpp
@@ -0,0 +1,20 @@
+#include <tonc.h>
+
+int main(){
+    // Gerenciamento de interrupção de hardware
+    irq_init(NULL);
+    irq_add(II_VBLANK, NULL);
+
+    // Mode de vídeo 0 e Fundo 0 ativados
+    REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
+    
+    while(TRUE){
+        // Update
+        key_poll(); // Pesquisa para keystates e chaves repetidas
+        
+        // Draw
+        VBlankIntrWait(); // Vsync
+
+    }
+    return 0;
+}
